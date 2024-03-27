f0cee946d9e36502c7255727a752c504c0399227
Greg Pavia <109236248+GregPavia@users.noreply.github.com>
2024-03-27 15:25:23 +0100

    Update Credentials.cs

--- nemseys/Models/Credentials.cs
+++ nemseys/Models/Credentials.cs
@@ -1,7 +1,9 @@
-﻿public class Credentials
+public class Credentials
 {
     public int UserId { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public byte[] Salt { get; set; }
+    public byte[] PasswordHash { get; set; } // Define PasswordHash property
     public int ProfileId { get; set; }
 }
