c1e468c0cfd9b0d65b2851f16b6bae529eca22e2
Greg Pavia <109236248+GregPavia@users.noreply.github.com>
2024-03-27 14:01:20 +0100

    Update profile.sql

--- nemseys/sql/profile.sql
+++ nemseys/sql/profile.sql
@@ -4,6 +4,7 @@ CREATE TABLE Profiles (
     Email NVARCHAR(100) NOT NULL UNIQUE,
     Phone NVARCHAR(20),
     ProfileType NVARCHAR(20) CHECK (ProfileType IN ('Reporter', 'Investigator'))
+    PasswordHash NVARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Credentials (
