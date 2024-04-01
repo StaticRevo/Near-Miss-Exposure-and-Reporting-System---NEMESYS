88e1e335f777982d92cf4dc856dd86ec908edd06
Greg Pavia <109236248+GregPavia@users.noreply.github.com>
2024-04-01 14:14:05 +0200

    Update Profile.cs

--- nemseys/Models/Profile.cs
+++ nemseys/Models/Profile.cs
@@ -1,8 +1,10 @@
-﻿public class Profile
+public class Profile
 {
     public int ProfileId { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
     public string ProfileType { get; set; }
+
+    public Credentials Credentials { get; set; }
 }
