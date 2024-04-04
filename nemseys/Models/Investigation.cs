d3f705ad81869e7969e4880dbaaaa6fb773b8e6d
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-04 15:56:26 +0200

    Add files via upload

---
+++ nemseys/Models/Investigation.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nemesis.Models
+{
+    public class Investigation
+    {
+        [Key]
+        public int InvestigationId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DateOfAction { get; set; }
+
+        // Foreign key
+        [ForeignKey("Investigator")]
+        public int InvestigatorId { get; set; }
+
+        // Navigation properties
+        public virtual Profile Investigator { get; set; }
+
+        [ForeignKey("Report")]
+        public int ReportId { get; set; }
+        public virtual Report Report { get; set; }
+    }
+}
+
---
+++ nemseys/Models/Report.cs
@@ -0,0 +1,47 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nemesis.Models
+{
+    public class Report
+    {
+        [Key]
+        public int ReportId { get; set; }
+
+        [Required]
+        public DateTime DateOfReport { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string HazardLocation { get; set; }
+
+        [Required]
+        public DateTime DateAndTimeSpotted { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TypeOfHazard { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        // Optional: For storing the path or URL to the photo
+        public string PhotoPath { get; set; }
+
+        public int Upvotes { get; set; }
+
+        // Foreign key
+        [ForeignKey("Reporter")]
+        public int ReporterId { get; set; }
+
+        // Navigation properties
+        public virtual Profile Reporter { get; set; }
+        public virtual Investigation Investigation { get; set; } // Assuming one-to-one relationship
+    }
+
+}
