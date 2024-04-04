e61fed48dcf02df6c90647c9f3b259ba1b843a14
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-04 16:47:25 +0200

    Update InvestigationViewModel.cs

--- nemseys/ViewModels/InvestigationViewModel.cs
+++ nemseys/ViewModels/InvestigationViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
 namespace Nemesis.ViewModels
 {
 	public class InvestigationViewModel
 	{
-		public InvestigationViewModel()
-		{
-			// For adding and viewing investigations
-		}
-	}
+        public int InvestigationId { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfAction { get; set; }
+        public int InvestigatorId { get; set; }
+        public string InvestigatorName { get; set; } 
+        public int ReportId { get; set; }
+        public string ReportTitle { get; set; } 
+
+        public InvestigationViewModel()
+        {
+            // For adding and viewing investigations
+        }
+
+    }
 }
 
