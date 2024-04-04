918662f4eeab79f9f26e221f673e831abd20ba0b
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-04 15:57:17 +0200

    Add files via upload

---
+++ nemseys/Models/Interfaces/IReportsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Nemesis.Models.Interfaces
+{
+    public interface IBloggyRepository
+    {
+        //IEnumerable<BlogPost> GetAllBlogPosts();
+        //BlogPost GetBlogPostById(int blogPostId);
+
+        //IEnumerable<Category> GetAllCategories();
+        //Category GetCategoryById(int categoryId);
+
+    }
+}
---
+++ nemseys/Models/Repositories/IInvestigationRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Nemesis.Models.Repositories
+{
+	public class IInvestigationRepository
+	{
+		public IInvestigationRepository()
+		{
+		}
+	}
+}
+
