8015778c56faae04b31b56596153821d7a7d690b
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-04 16:28:54 +0200

    Update ReportRepository.cs

--- nemseys/Models/Repositories/ReportRepository.cs
+++ nemseys/Models/Repositories/ReportRepository.cs
@@ -1,11 +1,50 @@
-﻿using System;
+using System;
+using Nemesis.Models.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Nemesis.Interfaces;
+
 namespace Nemesis.Models.Repositories
 {
-	public class ReportRepository
-	{
-		public ReportRepository()
+	public class ReportRepository: IReportRepository
+    {
+        private readonly AppDbContext _appDbcontext;
+
+        public ReportRepository(AppDbContext appDbContext)
 		{
+			_appDbcontext = appDbContext;
 		}
-	}
+
+        public IEnumerable<Report> GetAllReports()
+        {
+            return _appDbcontext.Reports.ToList();
+        }
+
+        public Report GetReportById(int reportId)
+        {
+            return _appDbcontext.Reports.Find(reportId);
+        }
+
+        public void AddReport(Report report)
+        {
+            _appDbcontext.Reports.Add(report);
+            _appDbcontext.SaveChanges();
+        }
+
+        public void UpdateReport(Report report)
+        {
+            _appDbcontext.Entry(report).State = EntityState.Modified;
+            _appDbcontext.SaveChanges();
+        }
+
+        public void DeleteReport(int reportId)
+        {
+            var report = _appDbcontext.Reports.Find(reportId);
+            if (report != null)
+            {
+                _appDbcontext.Reports.Remove(report);
+                _appDbcontext.SaveChanges();
+            }
+        }
+    }
 }
 
