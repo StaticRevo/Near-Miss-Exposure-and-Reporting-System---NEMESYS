99a47ea368a2f1b85ca3899c89be16dd309518e7
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-04 16:27:19 +0200

    Add files via upload

---
+++ nemseys/Models/Contexts/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nemesis.Models; 
+
+namespace Nemesis.Models.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Investigation> Investigations { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+           
+        }
+    }
+}
