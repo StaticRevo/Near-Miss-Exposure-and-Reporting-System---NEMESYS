f5fe5205caad1505adb4955bd58b5a02a345eb3c
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-15 17:43:58 +0200

    Update AppDbContext.cs

--- nemseys/Models/Contexts/AppDbContext.cs
+++ nemseys/Models/Contexts/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Nemesis.Models; 
 
 namespace Nemesis.Models.Contexts
@@ -13,9 +13,27 @@ namespace Nemesis.Models.Contexts
         public DbSet<Report> Reports { get; set; }
         public DbSet<Investigation> Investigations { get; set; }
         public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Uncategorised"
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Ex1"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "Ex2"
+                }
+            );
             base.OnModelCreating(modelBuilder);
            
         }
