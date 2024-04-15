975e5fe93629b1b0cb58200111c1f15c672d79d4
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-15 17:40:24 +0200

    Create ReportCategoryViewModel.cs

---
+++ nemseys/ViewModels/ReportCategoryViewModel.cs
@@ -0,0 +1,13 @@
+namespace Nemesis.ViewModels
+{
+    public class ReportCategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ReportCategoryViewModel()
+        {
+            
+        }
+    }
+}
