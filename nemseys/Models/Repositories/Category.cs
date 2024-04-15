fbc0b3476a97e70af749abb52d91cdaf746a7acb
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-15 17:43:00 +0200

    Delete nemseys/Models/Repositories/Category.cs

--- nemseys/Models/Repositories/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace Nemesis.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Report> Reports { get; set; }
-    }
-}
