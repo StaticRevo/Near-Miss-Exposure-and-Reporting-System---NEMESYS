f9c8291b254c553c11a390359ef7490907f786cf
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-20 12:26:42 +0200

    Delete nemseys/Models/Credentials.cs

--- nemseys/Models/Credentials.cs
+++
@@ -1,8 +0,0 @@
-public class Credentials
-{
-    public int UserId { get; set; }
-    public string Username { get; set; }
-    public byte[] Salt { get; set; }
-    public string PasswordHash { get; set; } // Define PasswordHash property
-    public int ProfileId { get; set; }
-}
