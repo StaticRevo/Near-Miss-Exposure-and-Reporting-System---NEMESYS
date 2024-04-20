9bd2bb526629e7773bb032f66f1c9c1746e1af53
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-20 12:25:56 +0200

    Update Report.cs

--- nemseys/Models/Report.cs
+++ nemseys/Models/Report.cs
@@ -1,54 +1,43 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Nemesis.Models
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Nemesys.Models
 {
     public class Report
     {
-        [Key]
+
         public int ReportId { get; set; }
 
-        [Required]
         public DateTime DateOfReport { get; set; }
 
-        [Required]
-        [StringLength(255)]
         public string HazardLocation { get; set; }
 
-        [Required]
+
         public DateTime DateAndTimeSpotted { get; set; }
 
-        [Required]
-        [StringLength(50)]
+
         public string TypeOfHazard { get; set; }
 
-        // This was missing in your initial model
-        [StringLength(255)]
+
         public string TitleOfReport { get; set; }
 
-        [Required]
+
         public string Description { get; set; }
 
-        [Required]
-        [StringLength(50)]
+
         public string Status { get; set; }
 
-        // This was missing in your initial model
         public string ImageUrl { get; set; }
 
         public int Upvotes { get; set; }
 
-        // Foreign key for the reporter
-        [ForeignKey("Reporter")]
-        public int ReporterId { get; set; }
-
-        // Navigation properties
-        public virtual Profile Reporter { get; set; }
-
-        // Assuming one-to-one relationship with Investigation
-        public virtual Investigation Investigation { get; set; }
-        public int CategoryId { get; set; }
-
-        public virtual Category Category { get; set; }
+        //Foreign Key - Navigation property
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
     }
 }
