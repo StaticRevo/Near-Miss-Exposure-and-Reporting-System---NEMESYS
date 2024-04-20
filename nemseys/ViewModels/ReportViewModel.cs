3322d72e2c13aafc23e1ac084eb2d74cc653420c
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-20 12:29:30 +0200

    Update ReportViewModel.cs

--- nemseys/ViewModels/ReportViewModel.cs
+++ nemseys/ViewModels/ReportViewModel.cs
@@ -1,23 +1,33 @@
-namespace Nemesis.ViewModels
+using Bloggy.ViewModels;
+using System;
+namespace Nemesys.ViewModels
 {
     public class ReportViewModel
     {
         public int ReportId { get; set; }
-        public DateTime DateOfReport { get; set; }
-        public string HazardLocation { get; set; }
-        public DateTime DateAndTimeSpotted { get; set; }
-        public string TypeOfHazard { get; set; }
-        public string TitleOfReport { get; set; }
-        public string Description { get; set; }
-        public string Status { get; set; }
-        public string ImageUrl { get; set; }
-        public string ReporterEmail { get; set; } 
-        public int Upvotes { get; set; }
-        public ReportCategoryViewModel Category { get; set; }
 
-        public ReportViewModel()
-        {
-            
-        }
+        public DateTime DateOfReport { get; set; }
+
+        public string HazardLocation { get; set; }
+
+
+        public DateTime DateAndTimeSpotted { get; set; }
+
+
+        public string TypeOfHazard { get; set; }
+
+
+        public string TitleOfReport { get; set; }
+
+
+        public string Description { get; set; }
+
+
+        public string Status { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Upvotes { get; set; }
+        public AuthorViewModel Author { get; set; }
     }
 }
