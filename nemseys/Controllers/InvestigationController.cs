45b0eab8125f31f46c3a0d1d223677fc20ffce1f
Isaac Attard <116385849+StaticRevo@users.noreply.github.com>
2024-04-28 13:29:03 +0200

    Update InvestigationController.cs

--- nemseys/Controllers/InvestigationController.cs
+++ nemseys/Controllers/InvestigationController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Bloggy.ViewModels;
 using System.Composition;
+using Microsoft.Extensions.Logging;
 
 
 namespace Nemesys.Controllers
@@ -15,11 +16,13 @@ namespace Nemesys.Controllers
     {
         private readonly INemeseysRepository _nemeseysRepository;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<InvestigationController> _logger;
 
-        public InvestigationController(INemeseysRepository bloggyRepository, UserManager<ApplicationUser> userManager)
+        public InvestigationController(INemeseysRepository bloggyRepository, UserManager<ApplicationUser> userManager, ILogger<InvestigationController> logger)
         {
             _nemeseysRepository = bloggyRepository;
             _userManager = userManager;
+            _logger = logger;
         }
 
         public IActionResult Index(string sortOrder)
@@ -43,6 +46,39 @@ namespace Nemesys.Controllers
             // Pass the sorted investigations to the view
             return View(investigations);
         }
+
+        public IActionResult Details(int id)
+        {
+            var investigation = _nemeseysRepository.GetInvestigationById(id);
+            if (investigation == null)
+                return NotFound();
+            else
+            {
+                var report = _nemeseysRepository.GetReportById(investigation.ReportId);
+                var model = new EditInvestigationViewModel()
+                {
+                    InvestigationId = investigation.InvestigationId,
+                    DateOfAction = investigation.DateOfAction,
+                    Status = investigation.Status,
+                    InvestigationTitle = investigation.InvestigationTitle,
+                    Outcome = investigation.Outcome,
+                    Feedback = investigation.Feedback,
+                    ReportId = investigation.ReportId,
+
+                    TitleOfReport = report.TitleOfReport,
+                    ReportDescription = report.Description,
+                    ImageUrl = report.ImageUrl,
+                    DateOfReport = report.DateOfReport,
+                    HazardLocation = report.HazardLocation,
+                    DateAndTimeSpotted = report.DateAndTimeSpotted,
+                    TypeOfHazard = report.TypeOfHazard,
+                    ReportStatus = report.Status,
+
+                };
+                return View(model);
+            }
+        }
+
         [Authorize(Roles = "Investigator")]
         [HttpGet]
         public IActionResult Create(int id)
@@ -64,6 +100,7 @@ namespace Nemesys.Controllers
                 DateOfAction = DateTime.Now,
                 Status = "Open",
                 TitleOfReport = report.TitleOfReport,
+                ReportDescription = report.Description,
                 ImageUrl = report.ImageUrl,
                 DateOfReport = report.DateOfReport,
                 HazardLocation = report.HazardLocation,
@@ -79,31 +116,274 @@ namespace Nemesys.Controllers
         [Authorize(Roles = "Investigator")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("ReportId, InvestigationTitle, DateOfAction, Status, Feedback")] Investigation newInvestigation)
+        public IActionResult Create(EditInvestigationViewModel newInvestigation)
         {
-           
+            _logger.LogInformation("Create POST method called");
             if (ModelState.IsValid)
             {
+                _logger.LogInformation("Model state is valid");
                 Investigation investigation = new Investigation()
                 {
                     ReportId = newInvestigation.ReportId,
                     InvestigationTitle = newInvestigation.InvestigationTitle,
                     DateOfAction = newInvestigation.DateOfAction,
                     Status = newInvestigation.Status,
+                    Outcome = newInvestigation.Outcome,
                     Feedback = newInvestigation.Feedback,
                     UserId = _userManager.GetUserId(User)
                 };
+                // Retrieve the report from the database
+                var report = _nemeseysRepository.GetReportById(newInvestigation.ReportId);
+
+                // Check if the report is null
+                if (report == null)
+                {
+                    // If the report is null, return a NotFound result
+                    return NotFound();
+                }
+
+                // Update the status of the report based on the status of the investigation
+                switch (investigation.Outcome)
+                {
+                    case "Resolved":
+                        report.Status = "Resolved";
+                        break;
+                    case "Unresolved":
+                        report.Status = "Under Review";
+                        break;
+                    case "Escalated":
+                        report.Status = "Under Review";
+                        break;
+                    // Add more cases as needed
+                    default:
+                        report.Status = "Unknown";
+                        break;
+                }
+
+                // Persist the changes to the report
+                _nemeseysRepository.UpdateReport(report);
+
                 // Persist to repository
-                _nemeseysRepository.CreateInvestigation(newInvestigation);
+                _nemeseysRepository.CreateInvestigation(investigation);
+                _logger.LogInformation("Investigation created successfully");
+                // Set a success message in TempData
+                TempData["Message"] = "Investigation created successfully!";
                 return RedirectToAction("Index", "Home");
             }
             else
             {
-                // If the model state is not valid, return to the view with the current model to allow for corrections
+                _logger.LogWarning("Model state is not valid");
+                // If the model state is not valid, retrieve the report data again
+
+                var report = _nemeseysRepository.GetReportById(newInvestigation.ReportId);
+                if (report != null)
+                {
+                    newInvestigation.TitleOfReport = report.TitleOfReport;
+                    newInvestigation.ImageUrl = report.ImageUrl;
+                    newInvestigation.DateOfReport = report.DateOfReport;
+                    newInvestigation.HazardLocation = report.HazardLocation;
+                    newInvestigation.DateAndTimeSpotted = report.DateAndTimeSpotted;
+                    newInvestigation.TypeOfHazard = report.TypeOfHazard;
+                    newInvestigation.ReportStatus = report.Status;
+                }
+
+                // Pass the updated model back to the view
                 return View(newInvestigation);
             }
         }
+        [Authorize]
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var existingInvestigation = _nemeseysRepository.GetInvestigationById(id);
+            if (existingInvestigation != null)
+            {
+                // Check if the current user has access to this resource
+                var currentUserId = _userManager.GetUserId(User);
+                if (existingInvestigation.UserId == currentUserId)
+                {
+                    var report = _nemeseysRepository.GetReportById(existingInvestigation.ReportId);
+                    var model = new EditInvestigationViewModel()
+                    {
+                        InvestigationId = existingInvestigation.InvestigationId,
+                        DateOfAction = existingInvestigation.DateOfAction,
+                        Status = existingInvestigation.Status,
+                        InvestigationTitle = existingInvestigation.InvestigationTitle,
+                        Outcome = existingInvestigation.Outcome,
+                        Feedback = existingInvestigation.Feedback,
+     
+                        TitleOfReport = report.TitleOfReport,
+                        ReportDescription = report.Description,
+                        ImageUrl = report.ImageUrl,
+                        DateOfReport = report.DateOfReport,
+                        HazardLocation = report.HazardLocation,
+                        DateAndTimeSpotted = report.DateAndTimeSpotted,
+                        TypeOfHazard = report.TypeOfHazard,
+                        ReportStatus = report.Status,
 
+                    };
+
+                    return View(model);
+                }
+                else
+                    return Forbid(); 
+            }
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Edit(int id, EditInvestigationViewModel updatedInvestigation)
+        {
+            var investigationToUpdate = _nemeseysRepository.GetInvestigationById(id);
+            if (investigationToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            // Check if the current user has access to this resource
+            var currentUserId = _userManager.GetUserId(User);
+            if (investigationToUpdate.UserId == currentUserId)
+            {
+                if (ModelState.IsValid)
+                {
+                    // Update investigation properties
+                    investigationToUpdate.DateOfAction = updatedInvestigation.DateOfAction;
+                    investigationToUpdate.Outcome = updatedInvestigation.Outcome;
+                    investigationToUpdate.Feedback = updatedInvestigation.Feedback;
+                    investigationToUpdate.Status = updatedInvestigation.Status;
+                    investigationToUpdate.InvestigationTitle = updatedInvestigation.InvestigationTitle;
+
+                    // Update the status of the associated report based on the outcome
+                    var report = _nemeseysRepository.GetReportById(investigationToUpdate.ReportId);
+                    if (report != null)
+                    {
+                        switch (updatedInvestigation.Outcome)
+                        {
+                            case "Resolved":
+                                report.Status = "Resolved";
+                                break;
+                            case "Unresolved":
+                            case "Escalated":
+                                report.Status = "Under Review";
+                                break;
+                            // Add more cases as needed
+                            default:
+                                report.Status = "Unknown";
+                                break;
+                        }
+                        _nemeseysRepository.UpdateReport(report);
+                    }
+                    _nemeseysRepository.UpdateInvestigation(investigationToUpdate);
+                    // Redirect to action and pass a flag indicating success
+                    TempData["Message"] = "Changes saved successfully!";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // If the model state is not valid, return to the view with the current model to allow for corrections
+                    return View(updatedInvestigation);
+                }
+            }
+            else
+                return Forbid(); // Or redirect to an error page or to the Index page
+        }
+
+        [Authorize(Roles = "Investigator")]
+        public IActionResult MyInvestigations()
+        {
+            var userId = _userManager.GetUserId(User);
+            
+            var investigations = _nemeseysRepository.GetAllInvestigations()
+                .Where(i => i.UserId == userId)
+                .OrderByDescending(i => i.DateOfAction);
+
+            var model = new InvestigationListViewModel()
+            {
+                TotalEntries = investigations.Count(),
+                Investigations = investigations.Select(i =>
+                {
+                    var report = _nemeseysRepository.GetReportById(i.ReportId);
+                    return new InvestigationViewModel
+                    {
+                        InvestigationId = i.InvestigationId,
+                        DateOfAction = i.DateOfAction,
+                        Status = i.Status,
+                        InvestigationTitle = i.InvestigationTitle,
+                        Outcome = i.Outcome,
+                        Feedback = i.Feedback,
+                        ReportId = i.ReportId,
+
+                        TitleOfReport = report.TitleOfReport,
+                        ReportDescription = report.Description,
+                        ImageUrl = report.ImageUrl,
+                        DateOfReport = report.DateOfReport,
+                        HazardLocation = report.HazardLocation,
+                        DateAndTimeSpotted = report.DateAndTimeSpotted,
+                        TypeOfHazard = report.TypeOfHazard,
+                        ReportStatus = report.Status,
+
+                        Author = new AuthorViewModel()
+                        {
+                            Id = i.UserId,
+                            Name = (_userManager.FindByIdAsync(i.UserId).Result != null) ?
+                            _userManager.FindByIdAsync(i.UserId).Result.UserName : "Anonymous"
+                        }
+                    };
+
+                })
+            };
+
+            return View(model);
+        }
+        // This method responds to GET requests and shows the confirmation page
+        [HttpGet]
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            var investigation = _nemeseysRepository.GetInvestigationById(id);
+            if (investigation == null)
+            {
+                return NotFound();
+            }
+
+            // Check if the current user has access to this resource
+            var currentUserId = _userManager.GetUserId(User);
+            if (investigation.UserId == currentUserId)
+            {
+                return View(investigation);
+            }
+            else
+            {
+                return Forbid(); // Or redirect to an error page or to the Index page
+            }
+        }
+
+        // This method responds to POST requests and actually deletes the investigation
+        [HttpPost, ActionName("Delete")]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var investigation = _nemeseysRepository.GetInvestigationById(id);
+            if (investigation == null)
+            {
+                return NotFound();
+            }
+
+            // Check if the current user has access to this resource
+            var currentUserId = _userManager.GetUserId(User);
+            if (investigation.UserId == currentUserId)
+            {
+                _nemeseysRepository.DeleteInvestigation(investigation);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return Forbid(); // Or redirect to an error page or to the Index page
+            }
+        }
 
     }
 }
