e0ca9f7b6c915b28cdbf1fa514fe6cd7e923687f
Greg Pavia <109236248+GregPavia@users.noreply.github.com>
2024-05-01 13:16:52 +0200

    Add files via upload

---
+++ nemseys/Views/Home/Support.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nemesys.Views.Home
+{
+    public class SupportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
