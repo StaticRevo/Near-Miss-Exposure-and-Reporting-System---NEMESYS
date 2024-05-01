752215f8c4e5d18bd42f1d11c760f2483b2c340a
Greg Pavia <109236248+GregPavia@users.noreply.github.com>
2024-05-02 01:43:47 +0200

    Update ErrorController.cs

--- nemseys/Controllers/ErrorController.cs
+++ nemseys/Controllers/ErrorController.cs
@@ -1,26 +1,44 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Nemesys.Models.Interfaces;
+using Nemesys.Models;
 
 namespace Nemesys.Controllers
 {
     public class ErrorController : Controller
     {
+        //Error controller handles error codes such as 404
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
 
         [Route("Error/{statusCode}")]
         public IActionResult Error(int? statusCode)
         {
-            if (statusCode.HasValue)
+            try //Error handling incase we dont have a page for a certain error code
             {
-                if (statusCode == 404)
+                if (statusCode.HasValue)
                 {
-                    return View("404");
-                }
-                else
-                {
-                    return View("404");
-                }
+                    if (statusCode == 404)
+                    {
+                        return View("404");
+                    }
+                    else
+                    {
+                        return View("404");
+                    }
 
+                }
+                return View();
             }
-            return View();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return View("Error");
+            }
         }
     }
 }
